f83377eddfd666cbbf9d1b4b3cc0df619f283db7
Retired <v.a.vsyakyy@student.khai.edu>
2024-12-26 17:50:30 +0200

    Добавьте файлы проекта.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult Calculate(double number1, double number2, string operation)
+        {
+            try
+            {
+                double result=0;
+                switch (operation)
+                {
+                    case "+":
+                        result = number1 + number2;
+                        break;
+                    case "-":
+                        result = number1 - number2;
+                        break;
+                    case "*":
+                        result = number1 * number2;
+                        break;
+                    case "/":
+                        if (number2 == 0)
+                            throw new ArgumentNullException();
+                        result = number1 / number2;
+                        break;
+
+                }
+                ViewBag.Result = result;
+               
+            }
+            catch (ArgumentNullException )
+            {
+                Console.WriteLine("Ділення на 0");
+            }
+            return View("Index");
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
